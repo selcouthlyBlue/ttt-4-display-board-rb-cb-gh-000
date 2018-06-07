683edd5d906b1eda964d7cb68ca00211dbc61381
Jerome Patrick Gonzalvo <jvgonzalvo@up.edu.ph>
2018-06-07 02:48:38 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board(board)
+def display_board(board, divider="-----------")
   puts display_row(board, 0)
+  puts divider
 end
 
 def display_row(board, row_number)
